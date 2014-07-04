d4156a0d6d4bee413b46b695c3d434bdb8ac52e4
Jonas Nyström <jonasnys@gmail.com>
2014-07-04 11:51:47 +0200

    Some instructive comments in example-minimal/src/App.hx

--- example-minimal/src/App.hx
+++ example-minimal/src/App.hx
@@ -6,6 +6,10 @@ class App extends React
 {	
 	static function main() 
 	{
+		// Here, the @dom metadata will invoke the React jsx magic, that will
+		// transform the xml into valid React javascript directives.
+		// The greeter attribute will be passed into the App class as a React props parameter .
+		// Finally, the resulting App component will be injected in the js.Browser.document.body element:
 		React.renderComponent(
 			@dom '<App greeter="world" />',
 			 js.Browser.document.body
